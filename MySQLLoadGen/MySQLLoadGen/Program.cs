9eeb1b9e5a6d8c68cfefc5bdff983204e0d65b37
seanli1988 <seal@microsoft.com>
2017-09-08 16:59:05 -0700

    2

--- MySQLLoadGen/MySQLLoadGen/Program.cs
+++ MySQLLoadGen/MySQLLoadGen/Program.cs
@@ -37,7 +37,7 @@ namespace MySQLLoadGen
             if (replica == "" || replica ==null)
             {
                 Console.WriteLine("No replica detected. Starting load generation on Master.");
-                mysqlslap m0 = new mysqlslap(process1, master + " --concurrency=" + workload + " --iterations=100  --query=\"select* from employees.current_dept_emp\"  --verbose --create-schema=employees");
+                mysqlslap m0 = new mysqlslap(process1, master + " --concurrency=" + workload + " --iterations=100  --query=\"select* from employees.dept_emp_latest_date\"  --verbose --create-schema=employees");
                 genload(m0);
             }
             else
@@ -45,8 +45,8 @@ namespace MySQLLoadGen
                 Console.WriteLine("Starting load generation on Master and Replica.");
                 process2 = new Process();
                 workload = workload / 2;
-                mysqlslap m1 = new mysqlslap(process1, master + " --concurrency=" + workload + " --iterations=100  --query=\"select* from employees.current_dept_emp\"  --verbose --create-schema=employees");
-                mysqlslap m2 = new mysqlslap(process2, replica + " --concurrency=" + workload + " --iterations=100  --query=\"select* from employees.current_dept_emp\"  --verbose --create-schema=employees");
+                mysqlslap m1 = new mysqlslap(process1, master + " --concurrency=" + workload + " --iterations=100  --query=\"select* from employees.dept_emp_latest_date\"  --verbose --create-schema=employees");
+                mysqlslap m2 = new mysqlslap(process2, replica + " --concurrency=" + workload + " --iterations=100  --query=\"select* from employees.dept_emp_latest_date\"  --verbose --create-schema=employees");
                 genload(m1);
                 genload(m2);
             }                        
